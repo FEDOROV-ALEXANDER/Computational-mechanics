c42749b433216473afc4c7d32ff86dd81e18878f
Александр Федоров <Fedorov.Alexander@live.ru>
2024-03-12 15:36:09 +0300

    Улучшил задание граничных условий и нагрузок

--- Euler–Bernoulli beam/Euler_Bernoulli_beam.py
+++ Euler–Bernoulli beam/Euler_Bernoulli_beam.py
@@ -1,14 +1,18 @@
 ﻿import matplotlib.pyplot as plt
 import numpy as np
 import array
+import module1 as m
 
 
 #enter data 
+# TODO: найти сове значение J. 
 L = 1 # meter 
 M = 1e4
 E = 2e11
 J = 1.63e-6#9.0625
 
+
+# TODO: попробывать сделать автоматический выбор размера элемента
 #number of elements 
 element_numbers = 20 
 #global rigidity matrix 
@@ -28,64 +32,40 @@ for i in range(element_numbers):
     sub = [2 * i, 2 * i + 1, 2 * i + 2, 2 * i + 3]
     K[np.ix_(sub, sub)] += k
 
-
-# forses and moments 
-
-node_forced = 1
+# forses and moments
 F = np.zeros((2 * element_numbers + 2))
-F[ 2 * node_forced - 1] = - M
+ 
 
-#boundary conditions
-bounded_node = 21
 
-K[2 * bounded_node - 1][:] = 0 
-K[:][2 * bounded_node -1] = 0
-K[2 * bounded_node - 2][:] = 0
-K[:][2 * bounded_node - 2] = 0 
-K[2 * bounded_node - 1][2 * bounded_node - 1] = 1
-K[2 * bounded_node - 2][2 * bounded_node - 2] = 1 
+loaded = m.Forse(1, "moment", M)
+F  = loaded.loading(F)
 
+right = m.Boundary(number = 21, type = "clamped")
+K = right.boundary_conditions(K)
 
 U = np.linalg.solve(K, F)
 
-def calc_deflection(u):
-    xsi = np.arange(-1, 1, 2)
-    ni = 1 / 4 * (1 - xsi) ** 2 * (2 + xsi)
-    ni_theta = 1 / 8 * l * (1 - xsi) ** 2 * (1 + xsi)
-    nj = 1 / 4 * (1 + xsi) ** 2 * (2 - xsi)
-    nj_theta = -1 / 8 * l * (1 + xsi) ** 2 * (1 - xsi)
-    n = np.array([ni, ni_theta, nj, nj_theta])
-    v = n.T.dot(u)
-    return v
 
-
-def calc_curvature(u):
-    xsi = np.arange(-1, 1, 2)
-    b = 1 / l * np.array([6 * xsi / l, 3 * xsi - 1, -6 * xsi / l, 3 * xsi + 1])
-    return b.T.dot(u)
-
-
-def calc_force(u):
-    b = 1 / l * np.array([6 / l, 3, -6 / l, 3])
-    return b.dot(u)
 
 
 x = np.linspace(0, 1, element_numbers)
 vs = []
 moments = []
 forces = []
-vs.extend(calc_deflection(U[0:4]))
-forces.append(-E * J * calc_force(U[0:4]))
-moments.extend(-E * J * calc_curvature(U[0:4]))
+vs.extend(m.calc_deflection(U[0:4]))
+forces.append(-E * J * m.calc_force(U[0:4]))
+moments.extend(-E * J * m.calc_curvature(U[0:4]))
 for i in range(2, U.shape[0] - 2, 2):
-    vs.extend(calc_deflection(U[i:i + 4]))
-    moments.extend(-E * J * calc_curvature(U[i:i + 4]))
-    forces.append(-E * J * calc_force(U[i:i + 4]))
+    vs.extend(m.calc_deflection(U[i:i + 4]))
+    moments.extend(-E * J * m.calc_curvature(U[i:i + 4]))
+    forces.append(-E * J * m.calc_force(U[i:i + 4]))
 
 Moments = array.array('f', moments)
 for i in range(element_numbers):
     print(x[i], Moments[i])
 
+
+# TODO:  Добавить сохранение данных в таблице и графики
 plt.figure()
 plt.plot(x, Moments) 
 plt.xlabel("координата, м")
--- Euler–Bernoulli beam/module1.py
+++ Euler–Bernoulli beam/module1.py
@@ -1,8 +1,67 @@
-import numpy as np
+п»їimport numpy as np
 import math
 import meshio
 import matplotlib as plt
 import pandas as pd 
+
+def calc_deflection(u):
+    xsi = np.arange(-1, 1, 2)
+    ni = 1 / 4 * (1 - xsi) ** 2 * (2 + xsi)
+    ni_theta = 1 / 8 * l * (1 - xsi) ** 2 * (1 + xsi)
+    nj = 1 / 4 * (1 + xsi) ** 2 * (2 - xsi)
+    nj_theta = -1 / 8 * l * (1 + xsi) ** 2 * (1 - xsi)
+    n = np.array([ni, ni_theta, nj, nj_theta])
+    v = n.T.dot(u)
+    return v
+
+
+def calc_curvature(u):
+    xsi = np.arange(-1, 1, 2)
+    b = 1 / l * np.array([6 * xsi / l, 3 * xsi - 1, -6 * xsi / l, 3 * xsi + 1])
+    return b.T.dot(u)
+
+
+def calc_force(u):
+    b = 1 / l * np.array([6 / l, 3, -6 / l, 3])
+    return b.dot(u)
+
+
+class Boundary:
+
+    def __init__(self, number, type):
+        self.number = number
+        self.type = type
+
+    def  boundary_conditions(self, k):
+        if self.type == "clamped":
+            #Р·Р°РґРµР»РєР°, РѕРіСЂР°РЅРёС‡РёРІР°РµС‚ РІСЃРµ СЃС‚РµРїРµРЅРё СЃРІРѕР±РѕРґС‹
+            k[2 * self.number - 1][:] = 0 
+            k[:][2 * self.number -1] = 0
+            k[2 * self.number - 2][:] = 0
+            k[:][2 * self.number - 2] = 0 
+            k[2 * self.number - 1][2 * self.number - 1] = 1
+            k[2 * self.number - 2][2 * self.number - 2] = 1 
+        elif self.type == "pinned":
+            #С€Р°СЂРЅРёСЂРЅР°СЏ РѕРїРѕСЂР°,РѕРіСЂР°РЅРёС‡РёРІР°РµС‚ С‚РѕР»СЊРєРѕ РїРµСЂРµРјРµС‰РµРЅРёСЏ, СЂР°Р·СЂРµС€Р°РµС‚ РїРѕРІРѕСЂРѕС‚С‹
+            k[2 * self.number - 2][:] = 0
+            k[:][2 * self.number - 2] = 0 
+            k[2 * self.number - 2][2 * self.number - 2] = 1
+        return k
+
+class Forse:
+
+    def __init__(self, number, type, load):
+        self.number  = number
+        self.type = type 
+        self.load = load
+
+    def loading(self, F):
+        if self.type == "moment":
+            F[ 2 * self.number - 1] = -self.load
+        elif self.type == "concentrated force":
+            F[ 2 * self.number - 2] = -self.load
+    #С‚СѓС‚ РІ РёРґРµР°Р»Рµ РґРѕР±Р°РІРёС‚СЊ РґР»СЏ СЂР°СЃРїСЂРµРґРµР»РЅРЅРѕР№ РЅР°РіСЂСѓР·РєРё Рё РјР± РµС‰Рµ С‡С‚Рѕ-С‚Рѕ, РїРѕРєР° С…Р·
+        return F
 
 def save_data(U,F, elements, nodes): 
 
@@ -25,12 +84,12 @@ def save_data(U,F, elements, nodes):
     forces = np.zeros((len(elements), 1)) 
     forces[:, 0] = F
 
-    data_forces = pd.DataFrame(forces, index=elements ,columns=[ "Сила, Н"]) 
-    data_displacemet = pd.DataFrame(displacement, index = nodes,  columns =[ "Ux, м", "Uy, м"] )
-    data_displacemet.index.name = "Номер шарнира"
-    data_forces.index.name = "Номер стержня"
-    data_displacemet.to_excel(r'C:\Users\Alexander\source\Учеба\3 курс\Вычислительная механика\МКЭ\Finite_element_method\Euler–Bernoulli beam\Bending.xlsx') 
-    data_forces.to_excel(r'C:\Users\Alexander\source\Учеба\3 курс\Вычислительная механика\МКЭ\Finite_element_method\Euler–Bernoulli beam\Bending.xlsx')
+    data_forces = pd.DataFrame(forces, index=elements ,columns=[ "РЎРёР»Р°, Рќ"]) 
+    data_displacemet = pd.DataFrame(displacement, index = nodes,  columns =[ "Ux, Рј", "Uy, Рј"] )
+    data_displacemet.index.name = "РќРѕРјРµСЂ С€Р°СЂРЅРёСЂР°"
+    data_forces.index.name = "РќРѕРјРµСЂ СЃС‚РµСЂР¶РЅСЏ"
+    data_displacemet.to_excel(r'C:\Users\Alexander\source\РЈС‡РµР±Р°\3 РєСѓСЂСЃ\Р’С‹С‡РёСЃР»РёС‚РµР»СЊРЅР°СЏ РјРµС…Р°РЅРёРєР°\РњРљР­\Finite_element_method\EulerвЂ“Bernoulli beam\Bending.xlsx') 
+    data_forces.to_excel(r'C:\Users\Alexander\source\РЈС‡РµР±Р°\3 РєСѓСЂСЃ\Р’С‹С‡РёСЃР»РёС‚РµР»СЊРЅР°СЏ РјРµС…Р°РЅРёРєР°\РњРљР­\Finite_element_method\EulerвЂ“Bernoulli beam\Bending.xlsx')
 
 
 
@@ -48,99 +107,14 @@ def save_data():
 
 
 
-g = 9.81 # ускорение свободного падения
-J = 7.87e-9 # момент инерции
-A = 144e-6 # Площадь поперечного сечения
-E = 2e11 # модуль упругости Юнга
-rho = 7900 # плотность (сталь)
-nodes_num = 21 # число узлов
-elements_num = nodes_num - 1 # число отрезков
-L = 1 # длина всей балки
-l = L / elements_num # длина конечных элементов балки
+g = 9.81 # СѓСЃРєРѕСЂРµРЅРёРµ СЃРІРѕР±РѕРґРЅРѕРіРѕ РїР°РґРµРЅРёСЏ
+J = 7.87e-9 # РјРѕРјРµРЅС‚ РёРЅРµСЂС†РёРё
+A = 144e-6 # РџР»РѕС‰Р°РґСЊ РїРѕРїРµСЂРµС‡РЅРѕРіРѕ СЃРµС‡РµРЅРёСЏ
+E = 2e11 # РјРѕРґСѓР»СЊ СѓРїСЂСѓРіРѕСЃС‚Рё Р®РЅРіР°
+rho = 7900 # РїР»РѕС‚РЅРѕСЃС‚СЊ (СЃС‚Р°Р»СЊ)
+nodes_num = 21 # С‡РёСЃР»Рѕ СѓР·Р»РѕРІ
+elements_num = nodes_num - 1 # С‡РёСЃР»Рѕ РѕС‚СЂРµР·РєРѕРІ
+L = 1 # РґР»РёРЅР° РІСЃРµР№ Р±Р°Р»РєРё
+l = L / elements_num # РґР»РёРЅР° РєРѕРЅРµС‡РЅС‹С… СЌР»РµРјРµРЅС‚РѕРІ Р±Р°Р»РєРё
 
-
-def calc_deflection(u):
-    xsi = np.array[-1, 1]
-    ni = 1 / 4 * (1 - xsi) ** 2 * (2 + xsi)
-    ni_theta = 1 / 8 * l * (1 - xsi) ** 2 * (1 + xsi)
-    nj = 1 / 4 * (1 + xsi) ** 2 * (2 - xsi)
-    nj_theta = -1 / 8 * l * (1 + xsi) ** 2 * (1 - xsi)
-    n = np.array([ni, ni_theta, nj, nj_theta])
-    v = n.T.dot(u)
-    return v
-
-
-def calc_curvature(u):
-    xsi = np.linspace(-1, 1)
-    b = 1 / l * np.array([6 * xsi / l, 3 * xsi - 1, -6 * xsi / l, 3 * xsi + 1])
-    return b.T.dot(u)
-
-
-def calc_force(u):
-    b = 1 / l * np.array([6 / l, 3, -6 / l, 3])
-    return b.dot(u)
-
-
-k = E * J / (l * l * l) * np.array([[12, 6 * l, -12, 6 * l],
-[6 * l, 4 * l * l, -6 * l, 2 * l * l],
-[-12, -6 * l, 12, -6 * l],
-[6 * l, 2 * l * l, -6 * l, 4 * l * l]])
-
-
-k_global = np.zeros((2 * nodes_num, 2 * nodes_num))
-
-
-for i in range(elements_num):
-    k_global[2 * i, 2 * i: 2 * i + 4] += k[0, :]
-    k_global[2 * i + 1, 2 * i: 2 * i + 4] += k[1, :]
-    k_global[2 * i + 2, 2 * i: 2 * i + 4] += k[2, :]
-    k_global[2 * i + 3, 2 * i: 2 * i + 4] += k[3, :]
-    k_global[2 * nodes_num - 2, :] = 0
-    4
-    k_global[:, 2 * nodes_num - 2] = 0
-    k_global[2 * nodes_num - 2, 2 * nodes_num - 2] = 1
-    k_global[0, :] = 0
-    k_global[:, 0] = 0
-    k_global[0, 0] = 1
-
-
-F1 = np.zeros((42, 1))
-F2 = np.zeros((42, 1))
-Pl = -2942
-F_local1 = -l / 2 * rho * A * g * np.array([[1], [l / 6], [1], [-l / 6]])
-F_local2 = l * Pl / 2 * np.array([[1], [l / 6], [1], [-l / 6]])
-F1[0:2] = F_local1[0:2]
-F1[0] = 0
-F1[40:42] = F_local1[2:4]
-F1[40] = 0
-F2[10:12] = F_local2[0:2]
-F2[20:22] = F_local2[2:4]
-
-
-for i in range(5, 9):
-    F2[2 * i + 2:2 * i + 4] = F_local2[0:2] + F_local2[2:4]
-for i in range(19):
-    F1[2 * i + 2:2 * i + 4] = F_local1[0:2] + F_local1[2:4]
-
-
-u = np.linalg.solve(k_global, F1 + F2)
-x = np.linspace(0, 1, nodes_num)
-vs = []
-moments = []
-forces = []
-vs.extend(calc_deflection(u[0:4]))
-forces.append(-E * J * calc_force(u[0:4]))
-moments.extend(-E * J * calc_curvature(u[0:4]))
-for i in range(2, u.shape[0] - 2, 2):
-    vs.extend(calc_deflection(u[i:i + 4])[1:])
-    moments.extend(-E * J * calc_curvature(u[i:i + 4])[1:])
-    forces.append(-E * J * calc_force(u[i:i + 4]))
-
-
-plt.figure(0)
-plt.plot(x, u) 
-plt.xlabel("координата, м")
-plt.ylabel("перемещение, мм")
-plt.title("Beam")
-plt.grid()
 
